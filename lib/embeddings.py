799171348e63becf18739a0dd20960f2df83ac39
Tony Chu <tonychu.yw@gmail.com>
2021-05-30 22:59:11 -0700

    Update embeddings

--- lib/embeddings.py
+++ lib/embeddings.py
@@ -18,12 +18,11 @@ from transformers import LongformerTokenizer, LongformerModel
 
 class Book2Vec:
 
-    def __init__(self, model="longformer"):
+    def __init__(self, tokenizer='allenai/longformer-base-4096', model='allenai/longformer-base-4096'):
 
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        if model == "longformer":
-            self.tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096', output_hidden_states=True)
-            self.model = LongformerModel.from_pretrained('allenai/longformer-base-4096').to(self.device)
+        self.tokenizer = LongformerTokenizer.from_pretrained(tokenizer)
+        self.model = LongformerModel.from_pretrained(model, output_hidden_states=True).to(self.device)
 
 #-----------------------------------------------------------------
 
@@ -33,15 +32,13 @@ class Book2Vec:
         outputs = self.model(input_ids)
 
         # extract last hidden state
-        # print(outputs.keys())
-        last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
-        # print(last_hidden_states.shape)
+        #last_hidden_states = outputs[0]
+        #doc_embedding = list(torch.mean(last_hidden_states[0], dim=0).squeeze().cpu().detach().numpy())
 
-        # get document embeddings
-        #pooling = torch.nn.MaxPool1d(768, stride=1)
-        #doc_embedding = pooling(last_hidden_states).squeeze().cpu().detach().numpy()
-        doc_embedding = list(torch.mean(last_hidden_states[0], dim=0).squeeze().cpu().detach().numpy())
-        #print(doc_embedding.shape)
+        # extract last 4 hidden states
+        last_four_layers = [outputs.hidden_states[i] for i in (-1, -2, -3, -4)]
+        cat_hidden_states = torch.cat(tuple(last_four_layers), dim=-1)
+        doc_embedding = list(torch.mean(cat_hidden_states, dim=1).squeeze().cpu().detach().numpy())
 
         return doc_embedding
 
@@ -56,10 +53,9 @@ class Book2Vec:
             doc_embedding = self._embed(book_i)
             doc_embeds.append(doc_embedding)
             iter += 1
-            if iter%500 == 0:
+            if iter%1000 == 0:
                 end = time.time()
                 print(iter, "- time:", round((end-start)//60), "min",  round((end-start)%60), "sec")
-
         end = time.time()
         print("Total runtime:", round((end-start)//60), "min",  round((end-start)%60), "sec")
 
@@ -71,6 +67,13 @@ class Book2Vec:
     def save_embeddings(cls, embeddings, filename):
         with open(filename, 'wb') as handle:
             pickle.dump(np.array(embeddings), handle)
+#-----------------------------------------------------------------
+
+    @classmethod
+    def load_embeddings(cls, filename):
+        with open(filename, 'rb') as handle:
+            embeddings = pickle.load(handle)
+        return embeddings
 
 #-----------------------------------------------------------------
 #  End of Class Book2Vec
